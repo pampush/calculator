95018e607371f086d1ce74091dad56199649474f
pampush <32041268+pampush@users.noreply.github.com>
2020-08-03 23:59:52 +0300

    add html and css

--- recursiveCalc.js
+++ recursiveCalc.js
@@ -14,7 +14,7 @@ let calc = {
   '+': function (a, b) {
     return a+b;
   },
-  '*': function(a,b) {
+  'x': function(a,b) {
     return a*b;
   },
   '-': function(a,b) {
@@ -23,30 +23,88 @@ let calc = {
   '/': function(a,b){
     return a/b
   },
-
-  rec: function() {
+  operate: function() {
     let num = this.tokens.shift();
     num = Number(num);
     let op = this.tokens.shift();
     let res = 0;
     if(!op)
       return num;
-    if(op == '*' || op == '/') {
+    if(op == 'x' || op == '/') {
       res = this[op](num, this.tokens.shift()); // '*'(a,b)
       this.tokens.unshift(res); 
-      res = this.rec();
+      res = this.operate();
     }
     if(op == '+')
-      res = this[op](num, this.rec());
+      res = this[op](num, this.operate());
     if(op == '-') { // -a = + (-a) 
       let next = -this.tokens.shift();
       this.tokens.unshift(num, '+', next);
-      res = this.rec();
+      res = this.operate();
     }
 
     return res;
+  },
+
+  validateOperand: function(char) { // leading zeros problem
+    this._expression += char;
+    let arr = this._expression.match(/\d+(\.\d+)?/g); // 1.12 valid num
+    let exc1 = this._expression.match(/\.\./g);
+    let exc2 = this._expression.match(/\d+\.\d+\./); // 1.2.
+    let exc3 = this._expression.match(/([^\d]\.)|^\./g); // ^.12 or 12+.12
+    if(exc1 || exc2 || exc3){
+      this._expression = this._expression.slice(0,-1);
+      return;
+    }
+    if(arr)
+      expressionNode.textContent += char;
+  },
+
+  validateOperator: function(char) {
+    this._expression += char;
+    let arr = this._expression.match(/[-+][-+]/g);
+    if(!arr)
+      expressionNode.textContent += char;
+    else
+      this._expression = this._expression.slice(0, -1);
+  },
+
+  clear: function() {
+    this._expression = '';
+    this.tokens = [];
+    expressionNode.textContent = '';
+    resultNode.textContent = '= 0';
+  },
+
+  backspace: function() {
+    if(expressionNode.textContent == '')
+      return;
+    this._expression = this._expression.slice(0, -1);
+    expressionNode.textContent = expressionNode.textContent.slice(0, -1);
+  },
+  
+  updateResult: function(num) {
+    resultNode.textContent = `= ${num}`;
   }
 }
-calc.expression = '1-1-10-2*6+10-1';
-calc.parseString();
-console.log(calc.rec());
+
+const digits = document.querySelectorAll('.digits'),
+      operators = document.querySelectorAll('.operators'),
+      resultNode = document.querySelector('.result'),
+      expressionNode = document.querySelector('.expression'),
+      equalNode = document.querySelector('.equal'),
+      clearNode = document.querySelector('.clear'),
+      backspaceNode = document.querySelector('.backspace'),
+      negativeSwitchNode = document.querySelector('.negative');
+
+
+for(const digitsElem of digits)
+digitsElem.addEventListener('click', () => calc.validateOperand(digitsElem.value));
+for(const operatorsElem of operators)
+operatorsElem.addEventListener('click', () => calc.validateOperator(operatorsElem.value));
+
+equalNode.addEventListener('click', () => { calc.parseString(); calc.updateResult(calc.operate())});
+
+//negativeSwitchNode.addEventListener('click', () => {calc.negativeSwitch();});
+clearNode.addEventListener('click', () => calc.clear()); 
+backspaceNode.addEventListener('click', () => calc.backspace());
